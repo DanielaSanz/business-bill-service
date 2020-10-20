3628b61745451d5fb4893456bd9f768fbb643ba3
Daniela Sanchez <47358877+reinamarla@users.noreply.github.com>
2020-10-19 23:13:52 -0300

    Se modifica test de BillController

--- src/test/java/com/business/billservice/controller/BillControllerTest.java
+++ src/test/java/com/business/billservice/controller/BillControllerTest.java
@@ -1,25 +1,27 @@
 package com.business.billservice.controller;
 
 
-import com.business.billservice.builder.BillBuilder;
-import com.business.billservice.builder.DetailsBuilder;
 import com.business.billservice.controller.http.BillResponse;
 import com.business.billservice.model.Detail;
-import com.business.billservice.model.dto.BillDTO;
-import com.business.billservice.model.dto.DetailDTO;
-import com.business.billservice.service.DetailServiceImp;
 import com.business.billservice.supplier.BillSupplier;
-import com.business.billservice.supplier.DetailsSupplier;
+import com.business.billservice.utils.ValidateId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -29,7 +31,6 @@ import static org.mockito.Mockito.when;
 class BillControllerTest {
 
     private final Integer VALID_ID = 1;
-    private final BillDTO billDTO = new BillDTO(1,"1","22/09/2020", "Daniela", "Sanchez", 1.105);
 
     private final Detail detail1 = new Detail("Termo", 1 , 2100.00, 2100.00, 2100.00, 2100.00 );
     private final Detail detail2 = new Detail("Mate", 2 , 510.00, 459.00, 1020.00, 918.00);
@@ -46,6 +47,9 @@ class BillControllerTest {
     @Mock
     BillSupplier billSupplier;
 
+    @Mock
+    ValidateId validateId;
+
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -53,12 +57,30 @@ class BillControllerTest {
 
     @Test
     void obtainBill_NoCaughtException_ReturnsOK() {
-/*
         when(billSupplier.apply(VALID_ID)).thenReturn(billResponse);
 
         ResponseEntity<BillResponse> responseEntity = sut.obtainBill(VALID_ID);
 
-        assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));*/
+        assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(IdArgumentSource.class)
+    void obtainBill_IdBillIsNullOrLessZero_ReturnsBadRequest() {
+        doThrow(IllegalArgumentException.class).when(billSupplier).apply(0);
+
+        ResponseEntity<BillResponse> responseEntity = sut.obtainBill(0);
+
+        assertThat(responseEntity.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+    }
+
+    static class IdArgumentSource implements ArgumentsProvider {
+
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
+            return Stream.of( null, -1, 0
+            ).map(Arguments::of);
+        }
     }
 
     @Test
